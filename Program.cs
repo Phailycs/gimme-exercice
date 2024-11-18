48861e84b24d852a4529b726f157d5d0450a083f
phailycs <felix.robin--mores@hotmail.com>
2024-11-18 02:14:04 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gimme_exercice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Salut salut c'est moi !");
+
+            // Récupérer la liste des exercices sportifs existants
+
+            // Dissocier le type et les exercices qui y sont reliés
+
+            // Récupération des jours disponibles
+
+            // Random des types d'exercices par jour
+
+            // Random des exercices par jour
+
+            // Afficher un tableau des jours de la semaine avec les exercices assignés
+        }
+    }
+}
